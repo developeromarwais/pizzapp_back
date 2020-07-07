f0ba7d25be19bb393c290dbce60854b462529196
developeromarwais <developer@omarwais.com>
2020-07-07 03:15:31 +0300

    register, login & logout

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
  
+Route::post('register', 'Auth\RegisterController@register');
+
+Route::post('login', 'Auth\LoginController@login');
+Route::middleware('auth:api')->post('logout', 'Auth\LoginController@logout');
 
 Route::get('pizzas', 'PizzaController@index');
 Route::get('pizzas/{pizza}', 'PizzaController@show');
